e03455f558f0cecdc016c380e7b352b4e8cc3ce1
unknown <aitorsimon@deusto.es>
2014-10-30 15:12:38 +0100

    Pruebele

---
+++ src/es/deusto/ssdd/tracker/main/MainServer.java
@@ -0,0 +1,11 @@
+package es.deusto.ssdd.tracker.main;
+
+import es.deusto.ssdd.tracker.model.GlobalManager;
+
+public class MainServer {
+
+	public static void main(String[] args) {
+		new GlobalManager();
+	}
+
+}
